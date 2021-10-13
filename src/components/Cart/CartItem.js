6fb6fa42a435d35c3a4a10a890f3db4ee1cae64b
dm1tro69 <dimolg22@gmail.com>
2021-10-13 20:26:30 +0300

    add and remove from cart

--- src/components/Cart/CartItem.js
+++ src/components/Cart/CartItem.js
@@ -1,7 +1,10 @@
 import classes from './CartItem.module.css';
+import {useDispatch} from "react-redux";
+import {cartActions} from "../../store/cart-slice";
 
 const CartItem = (props) => {
-  const { title, quantity, total, price } = props.item;
+  const { title, quantity, total, price, id } = props.item;
+  const dispatch = useDispatch()
 
   return (
     <li className={classes.item}>
@@ -17,8 +20,8 @@ const CartItem = (props) => {
           x <span>{quantity}</span>
         </div>
         <div className={classes.actions}>
-          <button>-</button>
-          <button>+</button>
+          <button onClick={() => dispatch(cartActions.removeItemFromCart(id))}>-</button>
+          <button onClick={() => dispatch(cartActions.addItemToCart({id, price, title}))}>+</button>
         </div>
       </div>
     </li>
